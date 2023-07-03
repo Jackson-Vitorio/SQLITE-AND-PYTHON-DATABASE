a6d08fdba9a85c2530d2821651dfca71caefb13e
Jackson-Vitorio <136825439+Jackson-Vitorio@users.noreply.github.com>
2023-07-03 19:24:35 -0300

    ultimo update usando slqlite, falta tratamento de css

--- aplicativo/views.py
+++ aplicativo/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render, redirect
 from aplicativo.forms import CarrosForm
 from aplicativo.models import Carros
+from django.core.paginator import Paginator
+
 
 # Create your views here.
 def home(request):
     data = {}
-    data["db"] = Carros.objects.all()
+    search = request.GET.get('search')
+    if search:
+         data['db'] = Carros.objects.filter(modelo__icontains=search)
+    else:
+        data['db'] = Carros.objects.all()
+    # data["db"] = Carros.objects.all()
+    #all = Carros.objects.all()
+    #paginator = Paginator(all, 10)
+    #pages = request.GET.get('page')
+    #data['db'] = paginator.get_page(pages)
     return render(request, 'index.html', data)
 
 
